b1a214023574388dd914fbb4f3cc0e2d856e9aa7
hazaredcookie <wwallace@ripple.com>
2023-02-26 03:06:41 -0500

    spelling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,7 +43,7 @@
   {/if}
 {:else}
   <p>✅ Success!</p>
-  <p>ℹ️ Click on each key to autocopy it's values to your clipboard</p>
+  <p>ℹ️ Click on each key to autocopy it's value to your clipboard</p>
   <Card {wallet} />
 {/if}
 
